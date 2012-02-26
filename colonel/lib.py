fab092190d225ab160befcf9d7a6dfeb9be3fcc6
Olivier Breuleux <breuleux@gmail.com>
2012-02-25 21:28:06 -0500

    class-ified some gates in lib.py

--- colonel/lib.py
+++ colonel/lib.py
@@ -39,93 +39,9 @@ class Distribute(CommonGateSpec):
                 {0})
 
 
-# def Distribute(n):
-#     """
-#     Returns a gate with one input port and n output ports. The data
-#     provided as input is retransmitted from all output ports (in other
-#     words, this gate allows data to go from one port to many others).
-#     """
-#     result_names = ['o{i}'.format(i = i)
-#                     for i in range(n)]
-
-#     def do(state, input):
-#         return ((0, None),
-#                 {name: input for name in result_names},
-#                 {0})
-
-#     deps_map = dmergei({(): {}},
-#                        {(name, REQ): {'input': REQ}
-#                         for name in result_names})
-
-#     return CommonGateSpec(
-#         name = '{n}X'.format(n = n),
-#         ports = ['input', 'error'] + result_names,
-#         starter = lambda: (0, None),
-#         deps_map = deps_map,
-#         triggers = [({'input': AVAIL, name: REQ}, do)
-#                     for name in result_names],
-#         description = """
-#         Outputs 'input' in 'rX', X ranging from 0 to {n}.
-#         """.format(n = n-1))
-
-
-
 ##################
 ### IfThenElse ###
 ##################
-
-# def if_do(state, cond = VOID, iftrue = VOID, iffalse = VOID):
-#     # Careful about the arguments: normally, only one of cond, iftrue
-#     # and iffalse is not going to be VOID, and it depends on the value
-#     # of the flow state, state[0], below. See also: IfThenElse.deps_map.
-#     fs = state[0]
-#     if fs == 0:
-#         # We are checking the condition. If the condition is true, we
-#         # will place ourselves in state 1, and the next time we
-#         # propagate we will ask for iftrue. If the condition is false,
-#         # we get state 2, and we will fetch iffalse.
-#         fs = 1 if cond else 2
-#         ret = VOID     # there is no output yet
-#         consumed = {0} # consume the condition (port #0)
-#     elif fs == 1:
-#         # The condition was true in the previous run. Return iftrue.
-#         fs = 0         # return to state 0 (next condition)
-#         ret = iftrue   # output iftrue
-#         consumed = {1} # consume iftrue (port #1)
-#     elif fs == 2:
-#         # The condition was false in the previous run. Return iffalse.
-#         fs = 0         # return to state 0 (next condition)
-#         ret = iffalse  # output iffalse
-#         consumed = {2} # consume iffalse (port #2)
-#     else:
-#         raise MPVMException['if/badstate'](
-#             "IfThenElse should not have internal"
-#             " state other than 0, 1 or 2...",
-#             )
-#     return ((fs, None),
-#             dict(out = ret),
-#             consumed)
-
-# IfThenElse = CommonGateSpec(
-#     name = 'if',
-#     ports = ['cond', 'iftrue', 'iffalse', 'out', 'error'],
-#     starter = lambda: (0, None),    # initialize to state 0 (the second element is unused)
-#     deps_map = {(): {},
-#                 (0, 'out', REQ): {0: REQ},  # state 0 -> ask for condition
-#                 (1, 'out', REQ): {1: REQ},  # state 1 -> ask for iftrue
-#                 (2, 'out', REQ): {2: REQ}}, # state 2 -> ask for iffalse
-#     triggers = [(0, {'out': REQ, 'cond': AVAIL}, if_do),
-#                 (1, {'out': REQ, 'iftrue': AVAIL}, if_do),
-#                 (2, {'out': REQ, 'iffalse': AVAIL}, if_do)],
-#     description = """
-#     IfThenElse implements a lazy If. It works as follows: first, we
-#     are in state 0 and we request the condition. Depending on whether
-#     the condition is true or false, we go to state 1 or 2. State 1
-#     requests the computation of the true branch, state 2 requests that
-#     of the false branch. The other branch is left unrequested, and
-#     thus its associated computations are not performed. The
-#     appropriate result is returned when available.
-#     """)
 
 class IfThenElseC(CommonGateSpec):
 
@@ -395,51 +311,11 @@ class Bottleneck(CommonGateSpec):
 
 
 
-# def Bottleneck(n):
-#     """
-#     Bottleneck(n) has n inputs and one output ('out'). All inputs are
-#     requested if out is requested. If any input arrives, it will be
-#     sent to the output port, and so on.
-
-#     The Bottleneck gate can be used to output sequentially inputs that
-#     arrive in parallel (IMPORTANT: the order of the sequence depends
-#     on the order in which inputs arrive).
-#     """
-
-#     inames = ['i{i}'.format(i = i) for i in range(n)]
-
-#     def do(state, **args):
-#         for i in range(n):
-#             iname = 'i{i}'.format(i = i)
-#             if args[iname] is not VOID:
-#                 return ((1, None),
-#                         {'out': args[iname]},
-#                         {i})
-#         raise MPVMException('commongate/no_input')(
-#             "The Bottleneck gate instance {this} is not supposed to get"
-#             " called if there is no available input!",
-#             this = rval
-#             )
-
-#     rval = CommonGateSpec(
-#         name = 'Bo{n}'.format(n = n),
-#         ports = inames + ['out'],
-#         starter = lambda: (0, None), # state is not used
-#         deps_map = {(): {},
-#                     ('out', REQ): {i: REQ for i in range(n)}},
-#         triggers = [({'out': REQ, name: AVAIL}, do)
-#                     for name in inames],
-#         description = Bottleneck.__doc__)
-
-#     return rval
-
-
-
 ################
 ### Sequence ###
 ################
 
-def Sequence(n):
+class Sequence(CommonGateSpec):
     """
     Sequence(n) has n inputs (i0, i1, ..., iN) and one output
     ('out'). Sequence(n) will first request i0 (and only i0). Then,
@@ -455,9 +331,25 @@ def Sequence(n):
     to i(n-1)).
     """
 
-    inames = ['i{i}'.format(i = i) for i in range(n)]
+    def __init__(self, n):
+        self.n = n
+        inames = ['i{i}'.format(i = i) for i in range(n)]
 
-    def do(state, **args):
+        deps_map = {(): {}}
+        for i in range(n):
+            # Each state corresponds to an input to request:
+            deps_map[(i, 'out', REQ)] = {i: REQ}
+
+        super().__init__(
+            name = 'Seq{n}'.format(n = n),
+            ports = inames + ['out'],
+            starter = lambda: (0, None), # second part of state is not used
+            deps_map = deps_map,
+            triggers = [(i, {'out': REQ, i: AVAIL}, self.do)
+                        for i in range(n)],
+            description = Sequence.__doc__)
+
+    def do(self, state, **args):
         currentn = state[0]
         # This is the current input, the one we just requested
         current = 'i{i}'.format(i = currentn)
@@ -465,23 +357,9 @@ def Sequence(n):
         # input to be requested, as per deps_map). If this was the
         # last input, we return it via the out port. We consume
         # (discard) the input we requested.
-        return (((currentn + 1) % n, None),
-                {'out': args[current] if currentn == n-1 else VOID},
+        return (((currentn + 1) % self.n, None),
+                {'out': args[current] if currentn == self.n-1 else VOID},
                 {current})
-
-    deps_map = {(): {}}
-    for i in range(n):
-        # Each state corresponds to an input to request:
-        deps_map[(i, 'out', REQ)] = {i: REQ}
-
-    return CommonGateSpec(
-        name = 'Seq{n}'.format(n = n),
-        ports = inames + ['out'],
-        starter = lambda: (0, None), # second part of state is not used
-        deps_map = deps_map,
-        triggers = [(i, {'out': REQ, i: AVAIL}, do)
-                    for i in range(n)],
-        description = Sequence.__doc__)
 
 
 
@@ -507,22 +385,6 @@ class Constant(CommonGateSpec):
     def do(self, state):
         return (state, dict(out = self.value), set())
 
-# def Constant(x):
-#     """
-#     Constant(x) has no input ports. It always returns x.
-#     """
-
-#     do = lambda state: (state, dict(out = x), set())
-
-#     return CommonGateSpec(
-#         name = '`{x}`'.format(x = x),
-#         ports = ['out'],
-#         starter = lambda: (0, None), # state is not used
-#         deps_map = {(): {},
-#                     ('out', REQ): {}},
-#         triggers = [({'out': REQ}, do)],
-#         description = Constant.__doc__)
-
 
 
 ############
@@ -545,45 +407,46 @@ NOOP = CommonGateSpec(
 ### Explode ###
 ###############
 
-def Explode(n):
+class Explode(CommonGateSpec):
 
-    result_names = ['o{i}'.format(i = i)
-                    for i in range(n)]
+    def __init__(self, n):
+        self.n = n
+        result_names = ['o{i}'.format(i = i)
+                        for i in range(n)]
+        self.result_names = result_names
 
-    def do(state, input):
-        if len(input) != n:
+        triggers = {name: REQ for name in result_names}
+        triggers['error'] = REQ
+
+        deps_map = {(): {}}
+        for name in result_names + ['error']:
+            deps_map[(name, REQ)] = {'input': REQ}
+
+        super().__init__(
+            name = 'Explode{n}'.format(n = n),
+            ports = ['input', 'error'] + result_names,
+            starter = lambda: (0, None),
+            deps_map = deps_map,
+            triggers = [({name: REQ, 'input': AVAIL}, self.do)
+                         for name in result_names],
+            description = """
+            Outputs 'input[X]' in 'rX', X ranging from 0 to {n}.
+            """.format(n = n-1))
+
+    def do(self, state, input):
+        if len(input) != self.n:
             err = MPVMException['explode/wrong_input_length'](
                 "Input {input} has the wrong length. Expected a list"
                 " of length {n}, not {n2}.",
                 input = input,
-                n = n,
+                n = self.n,
                 n2 = len(input)
                 )
             return ((0, None), {'error': err}, {0})
         return ((0, None),
                 {name: input[i]
-                 for i, name in enumerate(result_names)},
+                 for i, name in enumerate(self.result_names)},
                 {0})
-
-    triggers = {name: REQ for name in result_names}
-    triggers['error'] = REQ
-
-    deps_map = {(): {}}
-    for name in result_names + ['error']:
-        deps_map[(name, REQ)] = {'input': REQ}
-
-    rval = CommonGateSpec(
-        name = 'Explode{n}'.format(n = n),
-        ports = ['input', 'error'] + result_names,
-        starter = lambda: (0, None),
-        deps_map = deps_map,
-        triggers = [({name: REQ, 'input': AVAIL}, do)
-                     for name in result_names],
-        description = """
-        Outputs 'input[X]' in 'rX', X ranging from 0 to {n}.
-        """.format(n = n-1))
-
-    return rval
 
 
 
@@ -610,8 +473,17 @@ Gte = FunctionGateSpec("Gte", lambda a, b: a >= b)
 Sqrt = FunctionGateSpec("Sqrt", lambda a: a ** 0.5)
 
 
-def Join(n):
-    return FunctionGateSpec("Join{n}".format(n = n),
-                            lambda **args: [v for k, v in sorted(args.items()) if k.startswith('i')],
-                            input_ports = ['i{i}'.format(i = i)
-                                           for i in range(n)])
+
+class Join(FunctionGateSpec):
+
+    def __init__(self, n):
+        self.n = n
+        super().__init__("Join{n}".format(n = n),
+                         self.do,
+                         input_ports = ['i{i}'.format(i = i)
+                                        for i in range(n)])
+
+    def do(self, **args):
+        return [v
+                for k, v in sorted(args.items())
+                if k.startswith('i')]
